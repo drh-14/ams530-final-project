c78a5f108941f82101e30281940b871e4a1a4280
drh-14 <darrenh1134@gmail.com>
2025-11-21 20:02:33 -0500

    finished implementing grid generation algorithm, added gitignore to ignore pycache

--- grid.py
+++ grid.py
@@ -1,22 +1,15 @@
-import numpy as np
-def generate_grid(N, M, n, m):
-    # N x M grid, n row subbox, y col subbox
+import random
+
+def generate_grid(N, M, n, m, num_black, num_white):
     num_rows, num_cols = N // n, M // m
     grid = [[[] for _ in range(num_cols)] for _ in range(num_rows)]
-
-def place_points(num_points_black, num_points_white, grid: list[list[list[int]]]):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            num_points = num_points_black if (i + j) % 2 == 0 else num_points_white
-            x_start, x_end  = 0, 0
-            x_difference = x_end - x_start
-            y_start, y_end = 0, 0
-            y_difference = y_end - y_start
-            random_x = np.random.rand(num_points)
-            random_y = np.random.rand(num_points)
-            grid[i][0][j] = np.array(
-                [(x_difference * random_x[i] + x_start, 
-                  y_difference * random_y[i] + y_start) 
-                  for i in range(num_points)]
-                )
+            num_points = num_black if (i + j) % 2 == 0 else num_white
+            x_start, x_end  = j * m, (j + 1) * m
+            y_start, y_end = i * n, (i + 1) * n
+            random_x = [random.uniform(x_start, x_end)] * num_points
+            random_y = [random.uniform(y_start, y_end)] * num_points
+            for k in range(num_points):
+                grid[i][j].append((random_x[k], random_y[k]))     
     return grid
---
+++ main.py
@@ -0,0 +1,3 @@
+from grid import *
+
+print(generate_grid(60, 60, 6, 6, 1900, 100))
